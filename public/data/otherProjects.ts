12f4d6d7c33f21ce0bbfac6038255a6616caa723
Ridho Khalis <ridho@transfez.com>
2023-02-09 23:24:37 +0700

    other projects

---
+++ public/data/otherProjects.ts
@@ -0,0 +1,52 @@
+const otherProjects = [
+  {
+    title: "Project 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante.",
+    techStack: ["React", "Node", "MongoDB"],
+    githubLink: "",
+    liveLink: "",
+  },
+  {
+    title: "Project 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante.",
+    techStack: ["React", "Node", "MongoDB"],
+    githubLink: "",
+    liveLink: "",
+  },
+  {
+    title: "Project 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante.",
+    techStack: ["React", "Node", "MongoDB"],
+    githubLink: "",
+    liveLink: "",
+  },
+  {
+    title: "Project 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante.",
+    techStack: ["React", "Node", "MongoDB"],
+    githubLink: "",
+    liveLink: "",
+  },
+  {
+    title: "Project 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante.",
+    techStack: ["React", "Node", "MongoDB"],
+    githubLink: "",
+    liveLink: "",
+  },
+  {
+    title: "Project 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante.",
+    techStack: ["React", "Node", "MongoDB"],
+    githubLink: "",
+    liveLink: "",
+  },
+];
+
+export default otherProjects;
---
+++ public/data/projects.ts
@@ -0,0 +1,31 @@
+const projects = [
+  {
+    imageSrc: "/assets/images/projects/kanban.png",
+    title: "Kanban",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl a ultricies lacinia, nisl nisl aliquam nisl, eu aliqu, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl a ultricies lacinia, nisl nisl aliquam nisl, eu aliqu, ",
+    techStack: ["React", "Next.js", "Tailwind"],
+    githubLink: "",
+    liveLink: "",
+  },
+  {
+    imageSrc: "/assets/images/projects/designo.png",
+    title: "Designo",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl a ultricies lacinia, nisl nisl aliquam nisl, eu aliqu, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl a ultricies lacinia, nisl nisl aliquam nisl, eu aliqu, ",
+    techStack: ["React", "Next.js", "Tailwind"],
+    githubLink: "",
+    liveLink: "",
+  },
+  {
+    imageSrc: "/assets/images/projects/planets-fact-site.png",
+    title: "Planets Fact Site",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl a ultricies lacinia, nisl nisl aliquam nisl, eu aliqu, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl a ultricies lacinia, nisl nisl aliquam nisl, eu aliqu, ",
+    techStack: ["React", "Next.js", "Tailwind"],
+    githubLink: "",
+    liveLink: "",
+  },
+];
+
+export default projects;
