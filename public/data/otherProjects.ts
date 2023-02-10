cb051f79c29d711bda10eb191e038a8004050622
Ridho Khalis <ridho@transfez.com>
2023-02-10 23:48:14 +0700

    projects adjustment

--- public/data/otherProjects.ts
+++ public/data/otherProjects.ts
@@ -1,27 +1,42 @@
 const otherProjects = [
   {
-    title: "Typemaster",
+    title: "Furni-Web",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante.",
-    techStack: ["React", "Node", "MongoDB"],
+      "A web app that allows users to showcase, add and update furniture. ",
+    techStack: ["React", "Redux", "Javascript", "Tailwind", "Firebase"],
+    githubLink: "",
+  },
+  {
+    title: "Furni-Mobile",
+    description: "A mobile app that allows users to see furniture. ",
+    techStack: ["React-Native", "Expo", "GraphQL", "Apollo"],
+    githubLink: "",
+  },
+  {
+    title: "ColorSrc",
+    description:
+      "A web app that allows users to generate color palettes by type, and store them in their account.",
+    techStack: ["Vue", "Pinia", "Javascript", "Tailwind", "Heroku"],
+  },
+  {
+    title: "Calculator",
+    description: "A web app that can perform basic arithmetic operations.",
+    techStack: ["Javascript", "CSS", "HTML"],
+    githubLink: "",
+    liveLink: "",
+  },
+  {
+    title: "Typemaster",
+    description: "A landing page for a fictional keyboard.",
+    techStack: ["CSS", "HTML"],
     githubLink: "",
     liveLink: "",
   },
   {
     title: "Insure",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante.",
-    techStack: ["React", "Node", "MongoDB"],
+    description: "A landing page for a fictional insurance company.",
+    techStack: ["CSS", "HTML"],
     githubLink: "",
-    liveLink: "",
-  },
-  {
-    title: "Calculator",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante. Sed euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, vel aliquam nisl nisl eu ante.",
-    techStack: ["React", "Node", "MongoDB"],
-    githubLink: "",
-    liveLink: "",
   },
 ];
 
