28b95c4850edfb2e6cce0a5cf7134b5ac1b1540f
Ridho Khalis <ridho@transfez.com>
2023-02-12 13:19:22 +0700

    projects data adjustment

--- public/data/otherProjects.ts
+++ public/data/otherProjects.ts
@@ -2,41 +2,43 @@ const otherProjects = [
   {
     title: "Furni-Web",
     description:
-      "A web app that allows users to showcase, add and update furniture. ",
+      "A web app that allows users to showcase, add, read, update, and delete furniture data.",
     techStack: ["React", "Redux", "Javascript", "Tailwind", "Firebase"],
-    githubLink: "",
+    githubLink: "https://github.com/ridhokhalis99/Furni-Web",
   },
   {
     title: "Furni-Mobile",
     description: "A mobile app that allows users to see furniture. ",
     techStack: ["React-Native", "Expo", "GraphQL", "Apollo"],
-    githubLink: "",
+    githubLink: "https://github.com/ridhokhalis99/Furni-Mobile",
   },
   {
     title: "ColorSrc",
     description:
       "A web app that allows users to generate color palettes by type, and store them in their account.",
     techStack: ["Vue", "Pinia", "Javascript", "Tailwind", "Heroku"],
+    githubLink: "https://github.com/ridhokhalis99/colorsrc.git",
   },
   {
     title: "Calculator",
     description: "A web app that can perform basic arithmetic operations.",
     techStack: ["Javascript", "CSS", "HTML"],
-    githubLink: "",
-    liveLink: "",
+    githubLink: "https://github.com/ridhokhalis99/calculator",
+    liveLink: "https://ridhokhalis99.github.io/calculator/",
   },
   {
     title: "Typemaster",
     description: "A landing page for a fictional keyboard.",
     techStack: ["CSS", "HTML"],
-    githubLink: "",
-    liveLink: "",
+    githubLink: "https://github.com/ridhokhalis99/typemaster-prelaunch",
+    liveLink: "https://ridhokhalis99.github.io/typemaster-prelaunch/",
   },
   {
     title: "Insure",
     description: "A landing page for a fictional insurance company.",
     techStack: ["CSS", "HTML"],
-    githubLink: "",
+    githubLink: "https://github.com/ridhokhalis99/insure",
+    liveLink: "https://ridhokhalis99.github.io/insure/",
   },
 ];
 
--- public/data/projects.ts
+++ public/data/projects.ts
@@ -4,8 +4,8 @@ const projects = [
     title: "Kanban",
     description: `A web app to manage your tasks. You can create, edit, delete, and move tasks between columns. You can also toggle dark mode or light mode for your preferences. The data stored in your account so you can access it anytime and anywhere.`,
     techStack: ["Next.js", "React", "NestJs", "PostgreSQL", "Typescript"],
-    githubLink: "",
-    liveLink: "",
+    githubLink: "https://github.com/ridhokhalis99/kanban",
+    liveLink: "https://ridho-kanban.netlify.app/",
   },
   {
     imageSrc: "/assets/images/projects/designo.png",
@@ -13,8 +13,8 @@ const projects = [
     description:
       "Multi-page website for a design agency. It has a home page, about page, and contact page. It provides animation and optimization for better user experience.",
     techStack: ["Astro", "Sass", "Typescript"],
-    githubLink: "",
-    liveLink: "",
+    githubLink: "https://github.com/ridhokhalis99/designo",
+    liveLink: "https://ridho-designo.netlify.app/",
   },
   {
     imageSrc: "/assets/images/projects/planets-fact-site.png",
@@ -22,8 +22,8 @@ const projects = [
     description:
       "A website that displays information about the planets in our solar system. It provides details about each planet, such as its rotation time, revolution time, radius, and average temperature, internal structure, and surface geology of planets.",
     techStack: ["React", "Next.js", "Sass", "Typescript", "Framer-Motion"],
-    githubLink: "",
-    liveLink: "",
+    githubLink: "https://github.com/ridhokhalis99/Planets-Fact-Site",
+    liveLink: "https://ridho-planets-fact-site.netlify.app",
   },
 ];
 
