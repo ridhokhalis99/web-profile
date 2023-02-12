6cec89753be14511b8afc691333a8c348fc0dffb
Ridho Khalis <ridho@transfez.com>
2023-02-12 23:01:26 +0700

    responsive social container footer

---
+++ public/data/socialLinks.ts
@@ -0,0 +1,23 @@
+import Github from "../../src/icons/Github.astro";
+import Linkedin from "../../src/icons/Linkedin.astro";
+import Instagram from "../../src/icons/Instagram.astro";
+
+const socialLinks = [
+  {
+    name: "Github",
+    href: "https://github.com/ridhokhalis99",
+    component: Github,
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/ridho-khalis/",
+    component: Linkedin,
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/ridho_khalis/",
+    component: Instagram,
+  },
+];
+
+export default socialLinks;
